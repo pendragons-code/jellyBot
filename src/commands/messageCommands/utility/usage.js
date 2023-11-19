942538decdaacd44a5e468c20a24032d743ab13e
pendragons-code <pendragonscode@gmail.com>
2023-11-19 20:39:42 +0800

    weee

--- src/commands/messageCommands/utility/usage.js
+++ src/commands/messageCommands/utility/usage.js
@@ -1,1 +1,24 @@
 const { EmbedBuilder } = require("discord.js")
+const { cpuUsage } = require("os-utils")
+const { projectUsage } = require("../../../functions/usage/resourceUsage.js")
+const { defaults } = require("../../../../config.json")
+
+module.exports = {
+	name: "usage",
+	aliases: ["-u"],
+	category: "core",
+	utilisation: "usage",
+	desc: "Sends you details of the bot's resource usage.",
+	async execute(messageCreate, args, prefix) {
+		cpuUsage(async function(toEmbedCpuUsage) {
+			const usageEmbed = new EmbedBuilder()
+			usageEmbed.setColor(defaults.defaultEmbedColor)
+			usageEmbed.setFooter({ text: defaults.defaultFooterText })
+			usageEmbed.setTitle("Jellypan's usage details!")
+			usageEmbed.setTimestamp()
+			usageEmbed.setDescription(`${await projectUsage(toEmbedCpuUsage)}`)
+			return messageCreate.channel.send({ embeds: [usageEmbed] })
+		})
+	}
+}
+
--- src/functions/usage/resourceUsage.js
+++ src/functions/usage/resourceUsage.js
@@ -1,7 +1,7 @@
 async function projectUsage(toEmbedCpuUsage) {
 	const rawMemoryUsage = process.memoryUsage.rss() / 1024 / 1024
 	const estimatedMemoryUsage = Math.round(rawMemoryUsage)
-	let description = `\nCPU usage: ${Math.round(toEmbedCpuUsage)}%\nMemory: ${estimatedMemoryUsage}MB`
+	let description = `\nCPU usage: ${Math.round(toEmbedCpuUsage * 100) / 100}%\nMemory: ${estimatedMemoryUsage}MB`
 	return description
 }
 module.exports = { projectUsage }
